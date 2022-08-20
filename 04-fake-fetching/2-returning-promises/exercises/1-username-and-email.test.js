8c59a966f6fcfe7a9dc08f28ef589efd93318b0b
Daniel Halasz <halaszdaniel@gmail.com>
2022-08-20 13:47:32 +0200

    correction by Samir

--- 04-fake-fetching/2-returning-promises/exercises/1-username-and-email.test.js
+++ 04-fake-fetching/2-returning-promises/exercises/1-username-and-email.test.js
@@ -9,29 +9,30 @@ const { log } = labeledLogger();
 /**
  *
  */
-const usernameAndEmail = () => {};
+const usernameAndEmail = (id) => fetchUserById(id).then(user =>`${id}. ${user.username}, ${user.email}`);
+ 
 
 // --- test function ---
 
 describe("usernameAndEmail: returns a user's name", () => {
   it("gets user 2's name", () => {
     return usernameAndEmail(2).then((actual) => {
-      expect(actual).toEqual('2. Antonette, Sanna@melissa.tv');
+      expect(actual).toEqual('2. Antonette, Shanna@melissa.tv'); // email has been corrected 
     });
   });
   it("gets user 3's name", () => {
     return usernameAndEmail(4).then((actual) => {
-      expect(actual).toEqual('4. Karianne, Junianne.OConner@kory.org');
+      expect(actual).toEqual('4. Karianne, Julianne.OConner@kory.org'); // email has been corrected 
     });
   });
   it("gets user 4's name", () => {
     return usernameAndEmail(7).then((actual) => {
-      expect(actual).toEqual('7. Elwyn.Skiles, Telly.Hoeger@billy.biz');
+      expect(actual).toEqual('7. Elwyn.Skiles, Telly.Hoeger@billy.biz'); 
     });
   });
   it("gets user 8's name", () => {
     return usernameAndEmail(10).then((actual) => {
-      expect(actual).toEqual('10. Moriah.Stanton, Re.Padberg@karina.biz');
+      expect(actual).toEqual('10. Moriah.Stanton, Rey.Padberg@karina.biz'); // email has been corrected 
     });
   });
 });

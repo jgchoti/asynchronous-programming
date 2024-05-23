a8f358e7424d482f218c5b14534f9573e7ac5b4d
jgchoti <jgchotirat@gmail.com>
2024-05-23 11:50:38 +0200

    Update long-enough.js

--- 03-promises/exercises/long-enough.js
+++ 03-promises/exercises/long-enough.js
@@ -1,4 +1,4 @@
-import { labeledLogger } from '../../../lib/labeled-logger.js';
+import { labeledLogger } from '../../lib/labeled-logger.js';
 
 const { log, error } = labeledLogger();
 
@@ -9,21 +9,49 @@ const { log, error } = labeledLogger();
     if `reject` is called or an error is thrown, .catch's callback is executed
 
 */
+// const isLongEnoughExecutor = (resolve, reject) => {
+//   const userInput = prompt('enter something longer than 5 characters');
+//   if (userInput !== null && userInput.length > 5) {
+//     resolve('your input is long enough');
+//   } else {
+//     reject('your input is too short');
+//   }
+// }
 
-// resolve/reject based on length of user input
-_ _((_, _) => {
-  const userInput = prompt('enter something longer than 5 characters');
-  if (userInput !== null && userInput.length > 5) {
-    _('your input is long enough');
-  } else {
-    _('your input is too short');
-  }
-})
-  ._(resolvedValue => {
-    log('resolved value: ', resolvedValue);
-  })
-  ._(rejectionValue => {
-    log('rejected value: ', rejectionValue);
-  });
+// const logSuccess = (resolvedValue) => {
+//   log('resolved value: ', resolvedValue);
+// }
+
+// const logFailure = (rejectionValue) => {
+//   log('rejected value: ', rejectionValue);
+// }
+
+// // resolve/reject based on length of user input
+// new Promise(isLongEnoughExecutor)
+//   .then(logSuccess)
+//   .catch(logFailure);
 
 log('= = = =  the call stack is empty  = = = =');
+
+
+const isLongEnough = async() => {
+  // const userInput = prompt('enter something longer than 5 characters');
+  const userInput = 'cattttttt'
+  if (userInput !== null && userInput.length > 5) {
+    return('your input is long enough');
+  } else {
+    throw new error('your input is too short');
+  }
+}
+
+const checkIfLongEnough = async () => {
+try {
+  const result = await isLongEnough()
+  console.log(result)
+}
+catch {
+  console.log('not long enough')
+}
+}
+await checkIfLongEnough()
+await setTimeout(checkIfLongEnough, 3000)

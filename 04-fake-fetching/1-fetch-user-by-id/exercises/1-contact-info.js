f32cc8778873b9b316336d8e84dd74f4533decda
jgchoti <jgchotirat@gmail.com>
2024-05-23 11:50:26 +0200

    Update 1-contact-info.js

--- 04-fake-fetching/1-fetch-user-by-id/exercises/1-contact-info.js
+++ 04-fake-fetching/1-fetch-user-by-id/exercises/1-contact-info.js
@@ -9,6 +9,8 @@ const { log, error } = labeledLogger();
 
 const getContactInfo = (user) => {
     // write me!
+    // "2: Shana@melissa.tv, 010-692-6593 x09125, anastasia.net"
+    return `${user.id}: ${user.email}, ${user.phone}, ${user.website}`
 };
 
 const handleError = (err) => error(err);
@@ -19,25 +21,29 @@ log('fetching and processing user 2');
 fetchUserById(2)
     .then((user) => getContactInfo(user))
     // "2: Shana@melissa.tv, 010-692-6593 x09125, anastasia.net"
-    .then((contactInfo) => log(contactInfo))
+    .then((contactInfo) => log(contactInfo)) //6
     .catch((err) => error(err));
 
 log('fetching and processing user 5');
 fetchUserById(5)
     .then(getContactInfo)
     // "5: Lucio_Hettinger@annie.ca, (254)954-1289, demarco.info"
-    .then(_)
-    .catch(_);
+    .then(log) //7
+    .catch(handleError);
 
 log('fetching and processing user 7');
 fetchUserById(7)
-    ._(_)
+    .then(getContactInfo)
     // "7: Telly.Hoeger@billy.biz, 210.067.6132, elvis.io"
-    ._(_)
-    ._(_);
+    .then(log) 
+    .catch(handleError); // 8
+//Error: 500 - Internal Server Error: unable to complete request for user 7
 
 log('fetching and processing user 12 (there are only 10 users!)');
 // 404
-__;
-
-log('= = = =  the call stack is empty  = = = =');
+fetchUserById(12)
+    .then(getContactInfo)
+    .then(log) 
+    .catch(handleError); // 9
+//Error: 404 - Not Found: there is no user with id 12
+log('= = = =  the call stack is empty  = = = ='); //5

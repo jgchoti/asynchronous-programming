96b78ac357ca00c6ec43e032737c8645ace4c456
jgchoti <jgchotirat@gmail.com>
2024-05-23 11:50:25 +0200

    Update 2-address.js

--- 04-fake-fetching/1-fetch-user-by-id/exercises/2-address.js
+++ 04-fake-fetching/1-fetch-user-by-id/exercises/2-address.js
@@ -9,6 +9,7 @@ const { log, error } = labeledLogger();
 
 const getAddress = (user) => {
     // write me!
+    return `${user.id}: ${user.address.street}, ${user.address.city} ${user.address.zipcode}`
 };
 
 const handleError = (err) => error(err);
@@ -17,18 +18,37 @@ const handleError = (err) => error(err);
 
 log('fetching and processing user 9');
 // "9: Dayna Park, Bartholomebury 76495-3109"
-__;
+fetchUserById(9).then(getAddress).then(log).catch(handleError)
 
 log('fetching and processing user 8');
 // "8: Ellsworth Summit, Aliyaview 45169"
-__;
+fetchUserById(8).then(getAddress).then(log).catch(handleError)
 
 log('fetching and processing user 2');
 // "2: Victor Plains, Wisokyburgh 90566-7771"
-__;
+fetchUserById(2).then(getAddress).then(log).catch(handleError)
 
 log('fetching and processing user 0');
 // 404
-__;
+fetchUserById(0).then(getAddress).then(log).catch(handleError)
 
 log('= = = =  the call stack is empty  = = = =');
+
+
+
+//     name: 'Leanne Graham',
+//         username: 'Bret',
+//             email: 'Sincere@april.biz',
+//                 address: {
+//     street: 'Kulas Light',
+//         suite: 'Apt. 556',
+//             city: 'Gwenborough',
+//                 zipcode: '92998-3874',
+//                     geo: { lat: '-37.3159', lng: '81.1496' }
+// },
+// phone: '1-770-736-8031 x56442',
+//     website: 'hildegard.org',
+//         company: {
+//     name: 'Romaguera-Crona',
+//         catchPhrase: 'Multi-layered client-server neural-net',
+//             bs: 'harness real-time e-markets'

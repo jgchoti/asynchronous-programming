88da85bbcd7898cfce6ea6f086ec2667c1f546f7
jgchoti <jgchotirat@gmail.com>
2024-05-23 11:51:11 +0200

    Update 05-mixed-1.js

--- 02-timeouts-and-intervals/2-sync-vs-async/examples/05-mixed-1.js
+++ 02-timeouts-and-intervals/2-sync-vs-async/examples/05-mixed-1.js
@@ -6,25 +6,25 @@ const { log } = labeledLogger();
 const callback1 = () => {
     log('- end synchronous 1000 ms');
 };
-log('- begin synchronous 1000 ms');
-synchronousTimeout(callback1, 1000);
+log('- begin synchronous 1000 ms'); //1
+synchronousTimeout(callback1, 1000); //2
 
 const callback2 = () => {
     log('- end synchronous 0 ms');
 };
-log('- begin synchronous 0 ms');
-synchronousTimeout(callback2, 0);
+log('- begin synchronous 0 ms'); //3
+synchronousTimeout(callback2, 0); //4
 
 const callback3 = () => {
     log('+ execute async  1000 ms');
 };
-log('+ schedule async 1000 ms');
-setTimeout(callback3, 1000);
+log('+ schedule async 1000 ms'); //5
+setTimeout(callback3, 1000); //9
 
 const callback4 = () => {
     log('+ execute async  0 ms');
 };
-log('+ schedule async 0 ms');
-setTimeout(callback4, 0);
+log('+ schedule async 0 ms');//6
+setTimeout(callback4, 0); //8
 
-log('= = = =  the call stack is empty  = = = =');
+log('= = = =  the call stack is empty  = = = ='); //7
